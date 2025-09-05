0d139672945d0da6f11e2979baae2ab3591a2e0a
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-05 05:52:33 +0000

    make the fade in transition when the website loads with a 1 second delay

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,9 +92,11 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        "fade-in": "fade-in 0.6s ease-out forwards",
+        "fade-in": "fade-in 0.6s ease-out 1s forwards",
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config;
+
+    
