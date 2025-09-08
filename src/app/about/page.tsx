664ad9be9db01125fff9ccd5f5d4804ef1fa1c6e
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-08 07:28:54 +0000

    swap markus with kenichiro

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,10 +5,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const teamMembers = [
   { name: "Mr. Rahul Mishra", title: "Chairman, IJCC", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-08-08%20at%2012.04.51.jpeg", hint: "man face" },
+  { name: "Mr. Kenichiro Iwahori", title: "Advisor, Sasakawa Foundation, Japan", imageUrl: "https://www.ijcc.in/images/WhatsApp_Image_2025-04-26_at_12.32.28-removebg-preview.png", hint: "man face" },
   { name: "Mr. Tomoyuki Iwama", title: "Director Yakult India", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-08-23%20at%2003.34.36.jpeg", hint: "man face" },
   { name: "Mr.Markus", title: "MD Asahi Travels Japan", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-08-23%20at%2003.34.36%20(1).jpeg", hint: "man face" },
   { name: "Ms. Yoko Torii", title: "IJCC Travel Partner, Japan", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-05-09%20at%2017.40.09.jpeg", hint: "woman face" },
-  { name: "Mr. Kenichiro Iwahori", title: "Advisor, Sasakawa Foundation, Japan", imageUrl: "https://www.ijcc.in/images/WhatsApp_Image_2025-04-26_at_12.32.28-removebg-preview.png", hint: "man face" },
   { name: "Dr. Randeep Rakwal", title: "Professor, Tsukuba University, Japan", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-03-25%20at%2012.28.47.jpeg", hint: "man face" },
   { name: "Mr. Gajendra Badgujar", title: "Vice-Chairman (Strategy), Ex. Director FICCI", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-03-21%20at%2011.35.42.jpeg", hint: "man face" },
   { name: "Mr. Prakash Yadav", title: "Vice-Chairman (Corporate Affairs), MD, AJU Hotels", imageUrl: "https://www.ijcc.in/images/WhatsApp%20Image%202025-03-21%20at%2011.36.11.jpeg", hint: "man face" },
