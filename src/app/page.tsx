16baaa48835b32e4fa05b331fb5ecf6623d2e3e6
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-10 06:13:37 +0000

    change the photo of it to this

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ const partners = [
     { name: "AJU Hotels", href: "https://www.ajujapanesehotels.com/english/", logoUrl: "https://www.ajujapanesehotels.com/images/logo.png", hint: "company logo" },
     { name: "Veena Solar", href: "https://veenapower.com", logoUrl: "https://veenapower.com/wp-content/uploads/2021/04/log-top2.jpg", hint: "company logo" },
     { name: "Ahuja Residences", href: "https://ahujaresidences.com", logoUrl: "https://ahujaresidences.com/wp-content/uploads/2024/08/Ahuja-Logo_cropped.webp", hint: "company logo" },
-    { name: "The Japan Foundation", href: "https://nd.jpf.go.jp/", logoUrl: "https://nd.jpf.go.jp/wp-content/themes/jf-nd-pc/assets/img/logo.png", hint: "organization logo" },
+    { name: "The Japan Foundation", href: "https://nd.jpf.go.jp/", logoUrl: "https://nd.jpf.go.jp/wp-content/uploads/2022/07/JFND_logo_bgtransparent-1.png", hint: "organization logo" },
 ];
 
 const heroItems = [
