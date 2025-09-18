48f91451e29a904da7ca22ff5fdd2a2b84cd1f02
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-18 11:01:33 +0000

    use it to add the translation feature

--- src/components/google-translate.tsx
+++ src/components/google-translate.tsx
@@ -10,33 +10,37 @@ declare global {
 
 export function GoogleTranslate() {
   useEffect(() => {
-    // A cookie is used to remember the selected language
-    const googleTranslateScript = document.createElement('script');
-    googleTranslateScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-    googleTranslateScript.async = true;
-    document.body.appendChild(googleTranslateScript);
-    
+    const addScript = document.createElement('script');
+    addScript.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    addScript.async = true;
+    document.body.appendChild(addScript);
+
     window.googleTranslateElementInit = () => {
       new (window as any).google.translate.TranslateElement({
           pageLanguage: 'en',
-          includedLanguages: 'en,ja',
+          includedLanguages: 'en,ja', // English and Japanese
           layout: (window as any).google.translate.TranslateElement.InlineLayout.SIMPLE,
           autoDisplay: false
       }, 'google_translate_element');
     };
 
     return () => {
-        const scripts = document.getElementsByTagName('script');
-        for (let i = scripts.length - 1; i >= 0; i--) {
-            if (scripts[i].src.includes('translate.google.com')) {
-                scripts[i].remove();
-            }
+      // Clean up script and element on component unmount
+      const scripts = document.getElementsByTagName('script');
+      for (let i = scripts.length - 1; i >= 0; i--) {
+        if (scripts[i].src.includes('translate.google.com')) {
+          scripts[i].remove();
         }
-        const a:any = document.querySelector("#google_translate_element")
-        if (a) {
-          a.innerHTML = ""
-        }
-    }
+      }
+      const gtElement = document.getElementById('google_translate_element');
+      if (gtElement) {
+        gtElement.innerHTML = '';
+      }
+      const googTopFrame = document.querySelector('.goog-te-gadget-simple');
+      if (googTopFrame) {
+        googTopFrame.remove();
+      }
+    };
   }, []);
 
   return null;
