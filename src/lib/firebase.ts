e676dd88e4693b4f3428efdac6bbdf45ce00e6a3
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-22 06:36:33 +0000

    heres the api key for the authentication of firebase keep it in env file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,16 @@
+
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "indo-japan-chamber-hub",
-  "appId": "1:771398335372:web:b649d5fb28acba28fcb4a9",
-  "storageBucket": "indo-japan-chamber-hub.firebasestorage.app",
-  "apiKey": "AIzaSyBal5EXOrligIPVBLmSsbqUlGeZB68uzQU",
-  "authDomain": "indo-japan-chamber-hub.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "771398335372"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "ijcc-backend.firebaseapp.com",
+  projectId: "ijcc-backend",
+  storageBucket: "ijcc-backend.firebasestorage.app",
+  messagingSenderId: "858232635591",
+  appId: "1:858232635591:web:d8752c07457afadaff7ee2",
+  measurementId: "G-KRL866ZFY6"
 };
 
 
