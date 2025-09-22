4a005070d473bbb1cef12b0f717f00040a3fad60
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-22 06:47:35 +0000

    make sure the users now they sign in or sign up are stored in the databa

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "ijcc-backend.firebaseapp.com",
-  projectId: "ijcc-backend",
-  storageBucket: "ijcc-backend.firebasestorage.app",
-  messagingSenderId: "858232635591",
-  appId: "1:858232635591:web:d8752c07457afadaff7ee2",
-  measurementId: "G-KRL866ZFY6"
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
