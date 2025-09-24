579f710205fd71d1dd62ac0699089145a4869119
Nayan Mitra <nayanmitra844@gmail.com>
2025-09-24 06:02:49 +0000

    make the home page website logo a little bit more larger

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -157,7 +157,7 @@ export function AppHeader() {
       <div className="container flex h-24 items-center justify-between">
         <div className="flex items-center gap-4">
             <Link href="/" className="flex items-center gap-4">
-              <Image src={logoUrl} alt="Indo-Japan Chamber of Commerce Logo" width={140} height={140} className="h-20 w-auto" />
+              <Image src={logoUrl} alt="Indo-Japan Chamber of Commerce Logo" width={160} height={160} className="h-24 w-auto" />
             </Link>
         </div>
 
@@ -344,5 +344,3 @@ export function AppHeader() {
     </header>
   );
 }
-
-    
