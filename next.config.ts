67523ca05e89f89d71823a8ce9914ad414410ad2
Nayan Mitra <nayanmitra844@gmail.com>
2025-10-01 06:09:56 +0000

    add this ID - rzp_live_RO5WubAMtNDOop
    KEY SECRET - oeI4SD6I5mZ2YDxBm8UH

--- next.config.ts
+++ next.config.ts
@@ -254,6 +254,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+  env: {
+    NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
+  }
 };
 
 export default nextConfig;
