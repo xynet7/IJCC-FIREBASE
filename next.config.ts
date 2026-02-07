f8da11d3231bd608575995de82986cac04ad20c5
Nayan Mitra <nayanmitra844@gmail.com>
2026-02-07 18:25:31 +0000

    use it -https://i.pinimg.com/736x/d3/c6/be/d3c6beaef44875abe86ab436be930

--- next.config.ts
+++ next.config.ts
@@ -288,6 +288,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
